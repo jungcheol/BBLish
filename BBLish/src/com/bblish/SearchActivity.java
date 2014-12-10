bd34fedbb6c7fdcaccc32f57942136e12c752b14
jungcheol <jungcheol@wemakeprice.com>
2014-12-10 18:11:37 +0900

    141210

--- BBLish/src/com/bblish/SearchActivity.java
+++ BBLish/src/com/bblish/SearchActivity.java
@@ -13,7 +13,14 @@ import com.bblish.DatabaseHelper;
 import com.bblish.InfoClass;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,12 +28,18 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.GridView;
+import android.widget.ImageView;
 
 public class SearchActivity extends Activity {
 	WebView webView;
 	
 	DatabaseHelper db;
 	InfoClass info;
+	
+	Bitmap sp;
+	Cursor ic;
+	Cursor ric;
+	ImageView selectImage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +48,43 @@ public class SearchActivity extends Activity {
 		
 		Log.d("", "[BBLishMainActivity] SearchActivity-onCreate : 1111");
 		
-		GridView gridview = (GridView)findViewById(R.id.gridView1);
+		String[] img = {MediaStore.Images.Thumbnails._ID};
+		ic = managedQuery(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, img, null, null, MediaStore.Images.Thumbnails.IMAGE_ID + "");
+		String[] pq = {MediaStore.Images.Media.DATA};
+		ric = managedQuery(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, pq, null, null, null);
+		int count = ic.getCount();
+		if (count > 0) {
+			selectImage = (ImageView)findViewById(R.id.selectImage);
+			
+		}
+
 		
+//		String[] proj = {
+//				Images.Media._ID, 
+//				Images.Media.DATA, 
+//				Images.Media.DISPLAY_NAME
+//		};
+//		int[] idx = new int[proj.length];
+//		
+//		ContentResolver resolver = mContext.getContentResolver();
+//		Cursor cursor = Images.Media.query(resolver, Images.Media.EXTERNAL_CONTENT_URI, proj);
+//		
+//		if (cursor != null && cursor.moveToFirst()) {
+//			idx[0] = cursor.getColumnIndex(proj[0]);
+//			idx[1] = cursor.getColumnIndex(proj[1]);
+//			idx[2] = cursor.getColumnIndex(proj[2]);
+//			
+//			do {
+//				int photoID = cursor.getInt(idx[0]);
+//				String photoPath = cursor.getString(idx[1]);
+//				String displayName = cursor.getString(idx[2]);
+//				
+//				Log.d("", "[BBLishMainActivity] photoID : " + photoID);
+//				Log.d("", "[BBLishMainActivity] photoPath : " + photoPath);
+//				Log.d("", "[BBLishMainActivity] displayName : " + displayName);
+//				Log.d("", "[BBLishMainActivity] --");
+//			} while (cursor.moveToNext());
+//		}
 		
 //		webView = (WebView)findViewById(R.id.bbengWeb2);
 //		webView.setWebViewClient(new WebViewClient());
